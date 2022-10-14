508f20485136cbe8ae864b10faf15c1cb072fc3a
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-10-14 16:07:05 +0100

    Updated Icons

--- src/data.js
+++ src/data.js
@@ -4,21 +4,21 @@ export default {
         id: 1,
         title: "HTML & CSS",
         description: "Two of the core technologies for building Web pages. HTML provides the structure of the page, CSS the (visual and aural) layout, for a variety of devices.",
-        icon: "src/assets/HTML&CSS.svg",
+        icon: "public/HTMLIcon.png",
     },
 
     {
          id: 2,
          title: "Javascript",
         description: "JS is a lightweight, interpreted, object-oriented language with first-class functions. That' what makes your website attractive :)",
-         icon: "src/assets/JS_Icon.svg",
+         icon: "public/JSIcon.png",
     },
 
     {
        id: 3,
        title: "React JS",
        description: "Declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”.",
-       icon: "src/assets/React_Icon.svg",
+       icon: "public/ReactIcon.png",
     },
 
     // {
