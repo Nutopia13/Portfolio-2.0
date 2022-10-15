093b37015f4f322a7d0adec87a70adc2302ac420
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-10-15 19:58:40 +0100

    Implemented Web3 Forms

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,6 +4,28 @@ import email from '../assets/email.svg'
 
 const Contact = () => {
 
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "1564fb77-bdc9-44e7-a14d-7f30f86df76b");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      setResult(res.message);
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
+    }
+  };
 
   return (
     <div className='pt-8 lg:pt-16  bg-blue1 overflow-hidden relative'>
@@ -12,7 +34,7 @@ const Contact = () => {
        
         <h3 className='contact_title max-w-[1000px]  text-bright_yellow'> Would you like to discuss <span className='text-white'>the project ?</span></h3>
 
-        <form name="contact" action="https://api.web3forms.com/submit" method="POST" className='flex mt-14 flex-col space-y-5 w-fit'>
+        <form name="contact" onSubmit={onSubmit} action="https://api.web3forms.com/submit" method="POST" className='flex mt-14 flex-col space-y-5 w-fit'>
         <input type="hidden" name="access_key" value="1564fb77-bdc9-44e7-a14d-7f30f86df76b" />
           <div className='flex flex-wrap gap-4 md:gap-8'>
         <div className='first_input relative z-20'>
@@ -36,6 +58,9 @@ const Contact = () => {
         <button type='submit' className='btn3 relative z-20 hover:shadow-none ease-in  lg:max-w-[220px] max-w-[170px] duration-75  uppercase font-extrabold text-sm lg:text-lg bg-bright_yellow py-2.5 px-6 lg:py-4 lg:px-9'>Submit</button>
 
       </form>
+      <div className='pt-2 md:pt-6'>
+      <span className='font-oswald lg:text-xl text-[#13CE66]'>{result}</span>
+      </div>
       </div>
 
       <h3 className='contact_foot lg:hidden md:hidden mt-16 bg-pink text-white py-2 font-bold text-center uppercase'>- Let's Work Together - </h3>
