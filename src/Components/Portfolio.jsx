44330439aad8e0ee1982105d322ce55acbbf4695
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-10-17 18:48:36 +0100

    Weather Widget

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Portfolio_Component_Mob from './Portfolio_Component_Mob'
 import data from '../data'
-import { useRef } from "react";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -10,10 +9,14 @@ import '../index.css'
 import { Navigation } from "swiper";
 import Portfolio_Components from './Portfolio_Compononets'
 import Weather from './Weather';
+import {useState} from 'react'
+import sun from '../assets/sun.svg'
 
   
 
 const Portfolio = () => {
+
+  const [weatherButtonPop, setWeatherbuttonPop] = useState(false)
 
     const cards = data.portfolio.map(item => {
         return (
@@ -25,14 +28,21 @@ const Portfolio = () => {
         ) 
     })
   return (
+  
+    
     <div className='bg-center  md:pb-32 pb-8'>
+      <Weather trigger={weatherButtonPop} setTrigger = {setWeatherbuttonPop} />
       <div className='lg:pt-32 pt-16'>
-        <div>
+        <div className='flex items-center'>
           <h2 className='m-auto md:text-left md:max-w-[436px] md:ml-12 lg:ml-32 lg:max-w-[765px] max-w-[333px] portfolio_title'>Why people love <br/>
           <span className='text-bright_yellow'>my work </span></h2>
+          <motion.img 
+          whileHover={{ scale: 1.4 }}
+          whileTap={{ scale: 0.9 }}
+          src={sun} onClick ={() => setWeatherbuttonPop(true)} alt=""  className='relative hidden md:block  right-60 max-w-[80px]'/>
         </div>
-       
-        <Weather />
+        
+    
         <div
         className='hidden progress-bar overflow-x-scroll md:flex m-auto port_cont--slide  max-w-[350px] md:max-w-[750px] lg:max-w-full flex-row  lg:pt-16 pt-16'>
         {cards}
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,11 +1,91 @@
 import React from 'react'
+import CurrentWeather from './CurrentWeather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from "../API";
+import Search from './Search'
+import { useState } from "react";
+import umbrella from '../assets/umbrella.svg'
+import read from '../assets/read.svg'
+import close from '../assets/close.svg'
+import { motion } from "framer-motion"
 
-const Weather = () => {
-  return (
-    <div className='weather absolute hidden md:block'>
+const Weather = (props) => {
 
-    </div>
-  )
+   const [currentWeather, setCurrentWeather] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+  
+    Promise.all([currentWeatherFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      })
+      .catch(console.log);
+  };
+
+  const weatherVariants = {
+    offscreen: {
+      y: -200,
+      opacity: 0
+      
+    
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        bounce: 0.7,
+        duration: 3
+      }
+    },
+    offscreen_bg:{
+      opacity: 0
+
+    },
+
+    onscreen_bg:{
+      opacity: 1
+    }
+  };
+
+    
+  return (props.trigger) ? (
+    <motion.div 
+    variants={weatherVariants}
+    initial="offscreen_bg"
+    whileInView="onscreen_bg"
+    viewport={{ once: true }} 
+
+    className='absolute weather z-50  hidden md:block'>
+        <motion.div
+        variants={weatherVariants}
+
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true }} 
+        className='mx-auto mt-7 relative rounded-2xl weather_container max-w-[1000px] h-2/3 bg-white'>
+          <div className='flex justify-between'>
+            <Search onSearchChange={handleOnSearchChange} />
+             <img src={close} className='max-w-[10%] mr-12' alt="" onClick = {() => props.setTrigger(false)} />
+          </div>
+            
+
+            <div className='flex absolute top-72 max-w-[980px] mx-auto justify-between items-center'>
+              <img src={umbrella} alt="" className='w-[20%]' />
+              <img src={read} alt="" className='w-[20%]' />
+            </div>
+            {currentWeather && <CurrentWeather data={currentWeather} />}
+        </motion.div>
+        {props.children}
+    </motion.div>
+    
+  ) : '';
 }
 
 export default Weather
